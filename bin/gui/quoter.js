fe219d0dd927d1fd5d2d23e89450e435429f2c44
cvogel3434 <cvogel.a.m@gmail.com>
2022-12-02 16:24:28 -0600

    merging updates from repo. quotedash.js is clean

--- bin/gui/quoter.js
+++ bin/gui/quoter.js
@@ -6,14 +6,13 @@ var Titlebar=require('../bin/repo/gui/js/modules/vg-titlebar.js');
 
 //layouts
 var {stdbook}=require('../bin/repo/gui/js/layouts/vg-stdbook.js');
-var vcontrol=require('../bin/repo/gui/js/layouts/view-controller.js');
 
 var gentable=require('../bin/repo/gui/js/modules/vg-tables.js')
 var domtools=require('../bin/repo/gui/js/tools/vg-displaytools.js');
 
 //tools
 var {ObjList}=require('../bin/repo/tools/box/vg-lists.js');
-var {DropNote}=require('../bin/repo/gui/js/modules/vg-poppers.js');
+var {DropNote}=require('../bin/repo/gui/js/modules/vg-dropnote.js');
 
 //useful paths
 var {quotesls}=require('../bin/gui/storage/lstore.js');
